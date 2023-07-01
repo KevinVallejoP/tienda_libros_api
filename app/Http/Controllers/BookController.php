d44928356fa61025cb5349eada3eb5cef61fb63c
unknown <kevinvallejopiedrahitagit config --global user.name Your>
2023-07-01 11:27:57 -0500

    Creamos seeders, controladores, modulos, captura erroress, modelos

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book; 
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    public function index()
+    {
+        try {
+            $books = Book::all(); 
+
+            return response()->json(['success' => true, 'data' => $books]); 
+        } catch (Exception $e){
+            Log::error($e->getMessage(). ' line: ' . $e->getLine() . ' file: ' . $e->getFile()); 
+
+            return response()->json([
+                'success' => false,
+                'message' => 'Error de servidor',
+                'info' => [
+                    'info error' -> $e->getMessage(),
+                    'file' => $e->getFile(),
+                    'line' => $e->getline(),
+                    $e->getMessage(),
+                ]    
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+     }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,13 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function() {
     Route::get('saludo', function () {
-        return response()->json([
-            'success' => true,
-            'data' => [
-                'name' =>'Mario',
-                'email' => 'mario@gmail.com',
-                'age' => 25,
-            ]
-        ]);
+        return 'Hola'; 
     });
+
+    Route::apiResource('books', BookController::class);  
 });
